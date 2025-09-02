451589aad0896ab61d44d832c0cc7e3f724f7e72
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2025-09-02 14:57:26 +0300

    backend - add new api to get a specific group

--- backend/apps/rbac/urls.py
+++ backend/apps/rbac/urls.py
@@ -7,13 +7,14 @@ urlpatterns = [
 
     # Group management
     path("groups/", groups, name="groups"),
+    path("group/<uuid:id>/", groups, name="group"),
     path("groups/create/", create_group, name="create_group"),
     path("groups/update/", update_group, name="update_group"),
     path("groups/delete/", delete_group, name="delete_group"),
 
     # Permission management
-    path("permissions/", permissions, name="permission"),
-    path("permission/<uuid:id>/", permissions, name="permissions"),
+    path("permissions/", permissions, name="permissions"),
+    path("permission/<uuid:id>/", permissions, name="permission"),
     path("permissions/create/", create_permission, name="create_permission"),
     path("permissions/update/", update_permission, name="update_permission"),
     path("permissions/delete/", delete_permission, name="delete_permission"),
--- backend/apps/rbac/views/group_views.py
+++ backend/apps/rbac/views/group_views.py
@@ -41,10 +41,12 @@ def _group_payload(g: Group, include_permissions: bool = True):
 
 @require_GET
 @csrf_protect
-def groups(request):
-    """
-    List all groups with their permissions.
-    """
+def groups(request, id=None):
+    
+    if id is not None:
+        group = Group.objects.filter(pk=id).first()
+        return JsonResponse({"group" : _group_payload(group)})
+
     gs = Group.objects.all().order_by("name")
     return JsonResponse({"groups": [_group_payload(g) for g in gs]})
 
