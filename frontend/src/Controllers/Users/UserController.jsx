a71227f9864716d933f94caece70633bb1b75d9d
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2025-09-03 09:34:10 +0300

    frontend - add deleteUser in the UserController

--- frontend/src/Controllers/Users/UserController.jsx
+++ frontend/src/Controllers/Users/UserController.jsx
@@ -29,6 +29,32 @@ class UserController {
         return;
     }
 
+    static async deleteUser(user_id) {
+        try {
+
+            const { data } = await api.get("/accounts/csrf");
+            const csrfToken = data?.csrfToken;
+
+            const res = await api.post(
+                "accounts/users/delete/",
+                {
+                    "user_id": user_id
+                },
+                {
+                    headers: {
+                        "X-CSRFToken" : csrfToken
+                    }
+                }
+            )
+            console.log(res);
+            return res;
+
+        } catch (error) {
+            console.error(error.message);
+            return;
+        }
+    }
+
 }
 
 export default UserController;
