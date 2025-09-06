a914991a7b5833c6f551175265d06288d8e31162
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2025-09-06 20:41:02 +0300

    frontend - add new links and pages

--- frontend_ops_manager/src/app/app.routes.ts
+++ frontend_ops_manager/src/app/app.routes.ts
@@ -2,10 +2,18 @@ import { Routes } from '@angular/router';
 import { ShipmentsComponent } from './pages/shipments/shipments.component';
 import { VehiclesComponent } from './pages/vehicles/vehicles.component';
 import { RoutesComponent } from './pages/routes/routes.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LiveMapComponent } from './pages/live-map/live-map.component';
+import { AlertsComponent } from './pages/alerts/alerts.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'shipments', pathMatch: 'full' },
+
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
   { path: 'shipments', component: ShipmentsComponent },
   { path: 'vehicles', component: VehiclesComponent },
-  { path: 'routes', component: RoutesComponent }
+  { path: 'routes', component: RoutesComponent },
+  { path: 'live-map', component: LiveMapComponent },
+  { path: 'alerts', component: AlertsComponent },
+
 ];
---
+++ frontend_ops_manager/src/app/pages/live-map/live-map.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-live-map',
+  imports: [],
+  templateUrl: './live-map.html',
+  styleUrl: './live-map.css'
+})
+export class LiveMapComponent {
+
+}
