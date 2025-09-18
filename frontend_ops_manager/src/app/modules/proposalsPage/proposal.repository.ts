3e3bff3327ecf2acb21ae76b78f79ea984f67fec
MOUSTAFA HAYDAR <programmermoustafa@gmail.com>
2025-09-18 08:00:43 +0300

    frontend - add function to accept the proposal

--- frontend_ops_manager/src/app/modules/proposalsPage/proposal.repository.ts
+++ frontend_ops_manager/src/app/modules/proposalsPage/proposal.repository.ts
@@ -1,6 +1,7 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ProposalsResponse } from '../../shared/models/logistics.model';
+import { switchMap, tap } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class ProposalsRepository {
@@ -12,10 +13,36 @@ export class ProposalsRepository {
   }
 
   acceptProposal(shipmentId: string, proposedRouteId: string) {
-    return this.http.post(
-      `${this.API_BASE_URL}/api/agentic/shipments/${shipmentId}/apply/`,
-      { proposed_route_id: proposedRouteId },
-      { withCredentials: true }
-    );
+    return this.http.get('/api/accounts/csrf/', { withCredentials: true }).pipe(
+              switchMap(() => {
+                  const csrfToken = this.getCookie('csrftoken');
+                  return this.http.post(
+                    `${this.API_BASE_URL}/api/agentic/shipments/${shipmentId}/apply/`,
+                    { proposed_route_id: proposedRouteId },
+                    { 
+                      withCredentials: true,
+                      headers: { 'X-CSRFToken': csrfToken ?? '' }
+                     }
+                  );
+              }),
+              tap(res => {
+                  console.log('Login repository response:', res);
+                  if ((res as any)?.ok === true && (res as any)?.user) {
+                    localStorage.setItem('user', JSON.stringify((res as any).user));
+                    localStorage.setItem('isAuthenticated', 'true');
+                  }
+              })
+          );
+
+    
   }
+
+  private getCookie(name: string): string {
+          const matches = document.cookie.match(
+              new RegExp('(?:^|; )' + name + '=([^;]*)')
+          );
+          return matches ? decodeURIComponent(matches[1]) : '';
+      }
+
+
 }
